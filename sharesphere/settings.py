5637afa49d09ffacaa29fff72bf9eebb44f6b09c
Vishnudas p u <vishnudask410@gmail.com>
2024-06-29 18:51:54 +0530

    after googleauth and clearing secret and password problems

--- sharesphere/settings.py
+++ sharesphere/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k0n3f_5wc*x=_3p&$nwr0(1l+05^tqciori575z7fkjxvgvdg6'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
